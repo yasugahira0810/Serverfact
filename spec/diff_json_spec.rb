6a419dcae5c4a4fed918034ffad1afc2c9c109ff
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2017-03-14 12:32:33 +0000

    Implement diff_json spec.rb using content_as_json matcher

--- spec/diff_json_spec.rb
+++ spec/diff_json_spec.rb
@@ -1,44 +1,52 @@
 require 'spec_helper'
 require 'json'
 
-path = "nodes/#{ENV['TARGET_HOST']}"
+PATH = "nodes/#{ENV['TARGET_HOST']}"
 
-before_fact = open("#{path}/before.json") do |io|
+unless ENV['REVERSE'] == '1' then
+  BEFORE_JSON = "#{PATH}/before.json"
+  AFTER_JSON = "#{PATH}/after.json"
+else
+  BEFORE_JSON = "#{PATH}/after.json"
+  AFTER_JSON = "#{PATH}/before.json"
+end
+
+BEFORE_HASH = open(BEFORE_JSON) do |io|
   JSON.load(io)
 end
 
-after_fact = open("#{path}/after.json") do |io|
+AFTER_HASH = open(AFTER_JSON) do |io|
   JSON.load(io)
 end
 
-after_fact.each do |key1, val1|
+BEFORE_HASH.each do |key1, val1|
   if val1.is_a?(String) then
-    describe command("echo #{key1}: #{val1}") do
-      its(:stdout) { should match /#{before_fact[key1]}/ }
+    describe file(AFTER_JSON) do
+      its(:content_as_json) { should include(key1 => val1) }
     end
   elsif !val1.nil?
     val1.each do |key2, val2|
       if val2.is_a?(String) then
-        describe command("echo #{key2}: #{val2}") do
-          its(:stdout) { should match /#{before_fact[key1][key2]}/ }
+        describe file(AFTER_JSON) do
+          its(:content_as_json) { should include(key1 => include(key2 => val2)) }
         end
       elsif !val2.nil?
         val2.each do |key3, val3|
           if val3.is_a?(String) then
-            describe command("echo #{key3}: #{val3}") do
-              its(:stdout) { should match /#{before_fact[key1][key2][key3]}/ }
+            describe file(AFTER_JSON) do
+              its(:content_as_json) { should include(key1 => include(key2 => include(key3 => val3))) }
             end
           elsif !val3.nil?
             val3.each do |key4, val4|
               if val4.is_a?(String) then
-                describe command("echo #{key4}: #{val4}") do
-                  its(:stdout) { should match /#{before_fact[key1][key2][key3][key4]}/ }
+                describe file(AFTER_JSON) do
+                  its(:content_as_json) { should include(key1 => include(key2 => include(key3 => include(key4 => val4)))) }
                 end
               elsif !val4.nil?
                 val4.each do |key5, val5|
                   if val5.is_a?(String) then
-                    describe command("echo #{key5}: #{val5}") do
-                      its(:stdout) { should match /#{before_fact[key1][key2][key3][key4][key5]}/ }
+                    describe file(AFTER_JSON) do
+                      its(:content_as_json) { should include(key1 => include(key2 => include(key3 => include(key4 => include(key5 => val5))))) }
                     end
                   else
                     raise 'error'
